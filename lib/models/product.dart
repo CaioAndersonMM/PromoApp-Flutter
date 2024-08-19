781b2679bd3f4d47e34b03dc23729ff4dfb955d0
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-19 16:24:32 -0300

    feat: Add Product model with required fields

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String name;
+  final String imageUrl;
+  final String location;
+  final double price;
+
+  Product({
+    required this.name,
+    required this.imageUrl,
+    required this.location,
+    required this.price,
+  });
+}
