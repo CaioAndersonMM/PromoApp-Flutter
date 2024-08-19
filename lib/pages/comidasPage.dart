c2d29287912d6dd004734a042751a166afd1b8a8
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-19 16:26:47 -0300

    feat: Produtos são recebidos numa lista de Produtos e o Dialog adiciona o produto na lista

--- lib/pages/comidasPage.dart
+++ lib/pages/comidasPage.dart
@@ -1,99 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:meu_app/main.dart';
+import 'package:meu_app/widgets/product_item.dart';
 
-class ComidasPage extends StatelessWidget {
+class ComidasPage extends StatefulWidget {
   const ComidasPage({super.key});
+
+  @override
+  _ComidasPageState createState() => _ComidasPageState();
+}
+
+class _ComidasPageState extends State<ComidasPage> {
+  final List<ProductItem> _products = [
+    const ProductItem(name: 'Comida 1', imageUrl: 'https://via.placeholder.com/50', location: 'Loja A', price: 19.99),
+    const ProductItem(name: 'Comida 2', imageUrl: 'https://via.placeholder.com/50', location: 'Loja B', price: 29.99),
+    const ProductItem(name: 'Comida 3', imageUrl: 'https://via.placeholder.com/50', location: 'Loja B', price: 29.99),
+
+  ];
+
+  void _addProduct(ProductItem product) {
+    setState(() {
+      _products.add(product); //Adiciona product a lista
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Comidas',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+        title: const Text('Comidas', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
         backgroundColor: const Color.fromRGBO(0, 12, 36, 1),
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
       backgroundColor: const Color.fromRGBO(0, 12, 36, 1),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.add, color: Colors.white),
+                  onPressed: () {
+                    _showAddProductDialog(context);
+                  },
+                ),
+                const Text('Adicionar nova comida', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
           Expanded(
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                     Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.add, color: Colors.white),
-                          onPressed: () {
-                            // Ação ao clicar no botão de adicionar
-                          },
-                        ),
-                        const Text(
-                          'Adicionar nova comida',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Wrap(
-                    // spacing: 40.0,
-                    runSpacing: 10.0,
-                    children: <Widget>[
-                      ProductItem(
-                        name: 'Comida 1',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja A',
-                        price: 19.99,
-                      ),
-                      ProductItem(
-                        name: 'Comida 2',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja B',
-                        price: 29.99,
-                      ),
-                      ProductItem(
-                        name: 'Comida 3',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja C',
-                        price: 39.99,
-                      ),
-                      ProductItem(
-                        name: 'Comida 4',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja D',
-                        price: 49.99,
-                      ),
-                      ProductItem(
-                        name: 'Comida 5',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja E',
-                        price: 59.99,
-                      ),
-                      ProductItem(
-                        name: 'Comida 6',
-                        imageUrl: 'https://via.placeholder.com/50',
-                        location: 'Loja F',
-                        price: 69.99,
-                      ),
-                    ],
-                  ),
-                ],
+              child: Wrap(
+                runSpacing: 10.0,
+                children: _products,
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  void _showAddProductDialog(BuildContext context) {
+    String name = '';
+    String imageUrl = '';
+    String location = '';
+    double price = 0.0;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Adicionar Produto'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                onChanged: (value) {
+                  name = value;
+                },
+              ),
+              // TextField(
+              //   decoration: const InputDecoration(labelText: 'URL da Imagem'),
+              //   onChanged: (value) {
+              //     imageUrl = value;
+              //   },
+              // ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Localização'),
+                onChanged: (value) {
+                  location = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Preço'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  price = double.tryParse(value) ?? 0.0;
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Adicionar'),
+              onPressed: () {
+                if (name.isNotEmpty && location.isNotEmpty && price > 0) {
+                  _addProduct(ProductItem(name: name, imageUrl: "https://via.placeholder.com/50", location: location, price: price));
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
