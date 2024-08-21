94ea6696da5cc4bad6b139519bae6457c1b437e3
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-21 02:07:10 -0300

    feat: Adicionado opção de selecionar cidade, através do Drawer (Menu Hamburguer) e no header de seleção agora existem selectbox.
    Home se transformou em Class, e caixa de pesquisa foi removido e virou um componente.

--- lib/main.dart
+++ lib/main.dart
@@ -6,155 +6,241 @@ import 'pages/produtosPage.dart';
 import 'pages/eventosPage.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Promoapp',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Color.fromRGBO(0, 12, 36, 1),
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: SingleChildScrollView(
-            // Envolva o Column com SingleChildScrollView
-            child: Column(
-              children: <Widget>[
-                const SizedBox(height: 20),
-                Row(
-                  children: <Widget>[
-                    const Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.white,
-                              width: 2.0,
-                            ),
-                          ),
-                          labelText: 'Pesquisar',
-                          labelStyle: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 60,
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              const Color.fromARGB(255, 29, 48, 82),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        child: const Center(
-                          child: Icon(
-                            Icons.search,
-                            size: 30,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 15),
-                SizedBox(
-                  height: 180, // Ajuste a altura conforme necessário
-                  child: PageView(
-                    children: <Widget>[
-                      Image.network(
-                        'https://via.placeholder.com/400x200.png?text=Em+Alta',
-                        fit: BoxFit.cover,
-                      ),
-                      Image.network(
-                        'https://via.placeholder.com/400x200.png?text=Anuncio',
-                        fit: BoxFit.cover,
-                      ),
-                      Image.network(
-                        'https://via.placeholder.com/400x200.png?text=Sobre+Nós',
-                        fit: BoxFit.cover,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 5),
-                const Wrap(
-                  spacing: 20.0,
-                  runSpacing: 10.0,
-                  children: <Widget>[
-                    TypeItem(name: 'Comidas', destinationPage: ComidasPage()),
-                    TypeItem(name: 'Produtos', destinationPage: ProdutosPage()),
-                    TypeItem(name: 'Eventos', destinationPage: EventosPage()),
-                    // Adicione mais itens conforme necessário
-                  ],
-                ),
-                _buildSectionHeader('Produtos (-) Preço', 'Categorias (-) Todos'),
-                _buildProductGrid(),
-              ],
-            ),
-          ),
-        ),
-        backgroundColor: const Color.fromRGBO(0, 12, 36, 1),
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedCity = 'Selecione uma cidade';
+  List<String> cidades = ['Mossoró', 'Natal', 'Jucurutu', 'João Pessoa', 'Campina Grande', 'Fortaleza', 'Recife'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Promoapp',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(0, 12, 36, 1),
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              child: Text(
+                'Escolha uma cidade para ver as promoções',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ...cidades.map((cidade) {
+              return ListTile(
+                // Cria um item de lista para cada cidade
+                leading: const Icon(Icons.location_city),
+                title: Text(cidade),
+                onTap: () {
+                  setState(() {
+                    selectedCity = cidade;
+                  });
+                  Navigator.pop(context);
+                },
+              );
+            }).toList(),
+            // Adicione mais cidades conforme necessário
+          ],
+        ),
+      ),
+      
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 20),
+              Text(
+                'Cidade Selecionada: $selectedCity',
+                style: const TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              // _caixaPesquisa('Pesquisar promoções'),
+              const SizedBox(height: 15),
+              SizedBox(
+                height: 150,
+                child: PageView(
+                  children: <Widget>[
+                    Image.network(
+                      'https://via.placeholder.com/400x200.png?text=Em+Alta',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.network(
+                      'https://via.placeholder.com/400x200.png?text=Anuncio',
+                      fit: BoxFit.cover,
+                    ),
+                    Image.network(
+                      'https://via.placeholder.com/400x200.png?text=Sobre+Nós',
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Wrap(
+                spacing: 20.0,
+                runSpacing: 10.0,
+                children: <Widget>[
+                  TypeItem(name: 'Comidas', destinationPage: ComidasPage()),
+                  TypeItem(name: 'Produtos', destinationPage: ProdutosPage()),
+                  TypeItem(name: 'Eventos', destinationPage: EventosPage()),
+                  // Adicione mais itens conforme necessário
+                ],
+              ),
+              _buildSectionHeader('Produtos (-) Preço', 'Categorias (-) Todos'),
+              _buildProductGrid(),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: const Color.fromRGBO(0, 12, 36, 1),
+    );
+  }
+}
+
+Widget _caixaPesquisa(String placeholder) {
+  return Row(
+    children: <Widget>[
+      Expanded(
+        child: TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 2.0,
+              ),
+            ),
+            labelText: placeholder,
+            labelStyle: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      const SizedBox(width: 10),
+      SizedBox(
+        width: 60,
+        height: 60,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 29, 48, 82),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
+          child: const Center(
+            child: Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+
+}
+
 Widget _buildSectionHeader(String leftText, String rightText) {
-  return Container(
-    margin: const EdgeInsets.only(bottom: 10, top: 10),
-    color: const Color.fromRGBO(0, 87, 255, 1),
-    padding: const EdgeInsets.all(8.0),
-    child: Row(
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Text(
-            leftText,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+  String selectedSortOption1 = 'Mais Baratos';
+  String selectedSortOption2 = 'Tudo';
+
+  final List<String> sortOptions = ['Mais Baratos', 'Mais Recentes', 'Mais Comprados'];
+  final List<String> sortOptions2 = ['Tudo', 'Comidas', 'Produtos', 'Eventos'];
+
+
+  return StatefulBuilder(
+    builder: (BuildContext context, StateSetter setState) {
+      return Container(
+        margin: const EdgeInsets.only(bottom: 10, top: 10),
+        color: const Color.fromARGB(255, 255, 255, 255),
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: DropdownButton<String>(
+                  value: selectedSortOption1,
+                  items: sortOptions.map((String option) {
+                    return DropdownMenuItem<String>(
+                      value: option,
+                      child: Text(option),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedSortOption1 = newValue!;
+                    });
+                  },
+                ),
+              ),
             ),
-          ),
-        ),
-        const Spacer(),
-        Container(
-          alignment: Alignment.bottomRight,
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Text(
-            rightText,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: DropdownButton<String>(
+                  value: selectedSortOption2,
+                  items: sortOptions2.map((String option) {
+                    return DropdownMenuItem<String>(
+                      value: option,
+                      child: Text(option),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedSortOption2 = newValue!;
+                    });
+                  },
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      ],
-    ),
+      );
+    },
   );
 }
 
@@ -179,129 +265,42 @@ Widget _buildProductGrid() {
 }
 
 List<ProductItem> _getProducts() {
-    return [
-      const ProductItem(
-        name: 'Produto X',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja A',
-        price: 19.99,
-      ),
-      const ProductItem(
-        name: 'Produto 2',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja B',
-        price: 29.99,
-      ),
-      const ProductItem(
-        name: 'Produto 3',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja C',
-        price: 39.99,
-      ),
-      const ProductItem(
-        name: 'Produto 4',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja D',
-        price: 49.99,
-      ),
-      const ProductItem(
-        name: 'Produto 5',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja E',
-        price: 59.99,
-      ),
-      const ProductItem(
-        name: 'Produto 6',
-        imageUrl: 'https://via.placeholder.com/50',
-        location: 'Loja F',
-        price: 69.99,
-      ),
-    ];
-  }
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  return [
+    const ProductItem(
+      name: 'Produto X',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja A',
+      price: 19.99,
+    ),
+    const ProductItem(
+      name: 'Produto 2',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja B',
+      price: 29.99,
+    ),
+    const ProductItem(
+      name: 'Produto 3',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja C',
+      price: 39.99,
+    ),
+    const ProductItem(
+      name: 'Produto 4',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja D',
+      price: 49.99,
+    ),
+    const ProductItem(
+      name: 'Produto 5',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja E',
+      price: 59.99,
+    ),
+    const ProductItem(
+      name: 'Produto 6',
+      imageUrl: 'https://via.placeholder.com/50',
+      location: 'Loja F',
+      price: 69.99,
+    ),
+  ];
 }
