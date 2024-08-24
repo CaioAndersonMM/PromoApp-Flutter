39b0924b95521ce235ee9a954779221d6b9dddc1
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-08-24 03:34:30 -0300

    update: Melhoria visual e animação, design mais moderno

--- lib/widgets/type_item.dart
+++ lib/widgets/type_item.dart
@@ -1,21 +1,14 @@
-// widgets/type_item.dart
-
 import 'package:flutter/material.dart';
-import 'base_item.dart';
 
-class TypeItem extends BaseItem {
+class TypeItem extends StatelessWidget {
+  final String name;
   final Widget destinationPage;
 
   const TypeItem({
     super.key,
-    required String name,
+    required this.name,
     required this.destinationPage,
-  }) : super(
-          name: name,
-          padding: const EdgeInsets.all(20),
-          margin: const EdgeInsets.only(bottom: 10, top: 20),
-          borderRadius: 2,
-        );
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +20,47 @@ class TypeItem extends BaseItem {
         );
       },
       child: Container(
-        margin: margin,
-        padding: padding,
-        decoration: containerDecoration(),
-        child: Text(
-          name,
-          style: textStyle(),
-          textAlign: TextAlign.center,
+        margin: const EdgeInsets.all(10),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(50),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => destinationPage),
+            );
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            width: 80, 
+            height: 80,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: const LinearGradient(
+                colors: [Color.fromARGB(255, 4, 32, 61), Color(0xFF42A5F5)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                name,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
         ),
       ),
     );
