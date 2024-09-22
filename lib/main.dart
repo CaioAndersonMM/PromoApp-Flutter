8c13272810886ff0922078a6fedd1630496319d0
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-09-22 18:39:31 -0300

    feat: Implementado função para abertura da câmera

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:meu_app/pages/desejosPage.dart';
@@ -14,6 +16,8 @@ import 'controllers/my_home_page_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Get.put(MyHomePageController());
@@ -46,6 +50,29 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
   final MyHomePageController controller = Get.put(MyHomePageController());
+
+ Future<void> _pickImageFromCamera() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      final Directory appDir = await getApplicationDocumentsDirectory();
+      final String appPath = appDir.path;
+      final String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+      final String newFilePath = '$appPath/$fileName.png';
+
+      // Salva a imagem no diretório do app
+      final File savedImage = await File(pickedFile.path).copy(newFilePath);
+
+      // Armazena a URL do arquivo salvo no controlador (ou use onde precisar)
+      controller.imagePath.value = savedImage.path;
+
+      // Aqui você pode navegar para a página de criação de produto, por exemplo
+      Get.snackbar('Foto Capturada', 'A imagem foi salva com sucesso!');
+    } else {
+      Get.snackbar('Erro', 'Nenhuma imagem foi capturada.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -142,7 +169,7 @@ class MyHomePage extends StatelessWidget {
             ],
             currentIndex: controller.selectedIndex.value,
             selectedItemColor: const Color.fromARGB(255, 3, 26, 102),
-            onTap: (index) {
+            onTap: (index) async {
               if (controller.selectedCity.value == 'Selecione uma cidade') {
                 controller.showCitySelectionAlert();
               } else {
@@ -150,7 +177,8 @@ class MyHomePage extends StatelessWidget {
                 if (index == 0) {
                   Get.offNamed('/home');
                 } else if (index == 1) {
-                  // Navegar para a página de publicação
+                  print('Abrindo câmera...');
+                  await _pickImageFromCamera();
                 } else if (index == 2) {
                   Get.offNamed('/desejo');
                 }
