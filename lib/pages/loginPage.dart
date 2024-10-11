3e0292c13dec36371b7255eba1b9080294d1799d
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-10-11 18:27:23 -0300

    update: Melhoria visual na interface de login, com modal e campos de inputs mais sólidos

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -39,9 +38,10 @@ class _LoginPageState extends State<LoginPage> {
       Get.offNamed('/home');
     } else {
       Get.snackbar(
-        'Erro', 'Usuário ou senha inválidos!',
+        'Erro',
+        'Usuário ou senha inválidos!',
         backgroundColor: const Color.fromARGB(255, 3, 41, 117),
-        colorText: Colors.white,
+        colorText: const Color.fromARGB(255, 255, 255, 255),
         snackPosition: SnackPosition.TOP,
         borderRadius: 10,
         margin: const EdgeInsets.all(10),
@@ -59,71 +59,132 @@ class _LoginPageState extends State<LoginPage> {
                 fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
-      backgroundColor: const Color.fromRGBO(0, 12, 36, 1),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            const Text(
-              "As melhores ofertas e promoções estão esperando por você, não fique de fora!",
-              style: TextStyle(color: Colors.white, fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.white, fontSize: 18)),
-              // ignore: prefer_const_constructors
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: Colors.white, fontSize: 18)),
-              obscureText: true,
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(
-                    double.infinity, 50), // Largura máxima e altura fixa
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Image.asset(
+          //   'assets/background.jpg', // Adicione sua imagem de fundo aqui
+          //   fit: BoxFit.cover,
+          // ),
+          Container(
+            color: const Color.fromRGBO(0, 12, 36, 1),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Card(
                 shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(15), // Raio de borda menor
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                color: Color.fromARGB(255, 255, 255, 255),
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        "As melhores ofertas e promoções estão esperando por você, não fique de fora!",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 12, 14, 126),
+                            fontSize: 15),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 40),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: const TextStyle(
+                              color: Color.fromRGBO(3, 10, 24, 1)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                                color: Color.fromRGBO(3, 10, 24, 1)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.blue),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.1),
+                        ),
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: const TextStyle(
+                              color: Color.fromRGBO(3, 10, 24, 1)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(
+                                color: Color.fromRGBO(3, 10, 24, 1)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: const BorderSide(color: Colors.blue),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.1),
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          backgroundColor:
+                              const Color.fromARGB(255, 3, 41, 117),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          shadowColor: const Color.fromARGB(118, 3, 13, 196),
+                          elevation: 5,
+                        ),
+                        onPressed: _login,
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 255, 255, 255)),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          text: "Não possui uma conta? ",
+                          style: const TextStyle(
+                              color: Color.fromRGBO(0, 12, 36, 1),
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: "Cadastre-se!",
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 33, 106, 165),
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Get.toNamed('/cadastro');
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              onPressed: _login,
-              child: const Text('Login',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             ),
-            const SizedBox(height: 30),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: "Não possui uma conta? ",
-                style: const TextStyle(color: Colors.white, fontSize: 18),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "Cadastre-se!",
-                    style: const TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.underline,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Get.toNamed('/cadastro');
-                      },
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
