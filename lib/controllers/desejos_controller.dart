408002734c9c9fe970c9e9b977aad70d832f7af8
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-11-02 06:39:45 -0300

    feat: Adiciona escuta de mudanças em produtos e atualiza a interface ao carregar desejos

--- lib/controllers/desejos_controller.dart
+++ lib/controllers/desejos_controller.dart
@@ -1,3 +1,4 @@
+// DesejosController
 import 'package:get/get.dart';
 import 'package:meu_app/controllers/my_home_page_controller.dart';
 import 'package:meu_app/models/product_item.dart';
@@ -8,20 +9,20 @@ class DesejosController extends GetxController {
   var desejos = <ProductItem>[].obs;
   final ProductSaveService productSaveService = ProductSaveService();
   final String userId = AuthService().getUserId();
-  final MyHomePageController controller = Get.find<MyHomePageController>(); // Obtenha a instância correta
+  final MyHomePageController controller = Get.find<MyHomePageController>();
 
   @override
   void onInit() {
     super.onInit();
     loadDesejos();
+    ever(controller.allproducts, (_) => loadDesejos()); // Escuta mudanças em produtos
   }
 
   Future<void> loadDesejos() async {
     try {
       List<String> favoriteProductIds = await productSaveService.obterProdutosFavoritados(userId);
-
       desejos.assignAll(controller.getProductsByIds(favoriteProductIds));
-      
+      update(); // Atualiza a interface quando a lista é alterada
     } catch (e) {
       print('Erro ao carregar desejos: $e');
     }
